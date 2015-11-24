a85173fbf1373c9c87f69d6abbed33b32500696d
Ivan Lavrov <lavrov@rpcenrt.com>
2015-11-24 17:05:46 +0300

    Shell view model

--- shell-demo/Shell/ViewModels/ShellViewModel.cs
+++ shell-demo/Shell/ViewModels/ShellViewModel.cs
@@ -4,11 +4,12 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 using Contracts;
 
 namespace Shell.ViewModels
 {
-    class ShellViewModel
+    class ShellViewModel: PropertyChangedBase
     {
         public ShellViewModel()
         {
@@ -16,5 +17,25 @@ namespace Shell.ViewModels
         }
 
         public ObservableCollection<ShellMenuItem> MenuItems { get; private set; }
+
+        private ShellMenuItem _selectedMenuItem;
+        public ShellMenuItem SelectedMenuItem
+        {
+            get { return _selectedMenuItem; }
+            set
+            {
+                if(_selectedMenuItem==value)
+                    return;
+                _selectedMenuItem = value;
+                NotifyOfPropertyChange(() => SelectedMenuItem);
+                NotifyOfPropertyChange(() => CurrentView);
+            }
+        }
+
+        public object CurrentView
+        {
+            get { return _selectedMenuItem == null ? null : _selectedMenuItem.ScreenViewModel; }
+        }
+
     }
 }
