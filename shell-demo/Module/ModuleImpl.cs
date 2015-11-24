e6fd0320f8a2cf3bb8029beef2b2e4f079f1fec7
Ivan Lavrov <lavrov@rpcenrt.com>
2015-11-24 17:20:39 +0300

    module created

---
+++ shell-demo/Module/ModuleImpl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Contracts;
+using Module.ViewModels;
+
+namespace Module
+{
+    class ModuleImpl : IModule
+    {
+        private readonly IShell _shell;
+        private readonly FirstViewModel _firstViewModel;
+
+        public ModuleImpl(IShell shell, FirstViewModel firstViewModel)
+        {
+            _shell = shell;
+            _firstViewModel = firstViewModel;
+        }
+
+        public void Init()
+        {
+            _shell.MenuItems.Add(new ShellMenuItem() {Caption = "First", ScreenViewModel = _firstViewModel});
+        }
+    }
+}
---
+++ shell-demo/Module/ModuleInstaller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Contracts;
+using Module.ViewModels;
+
+namespace Module
+{
+    public class ModuleInstaller:IWindsorInstaller 
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container
+                .Register(Component.For<FirstViewModel>())
+                .Register(Component.For<IModule>().ImplementedBy<ModuleImpl>());
+        }
+    }
+}
